57eb5a656b703860a155c0d0744c489490dbdb14
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-12-04 09:48:50 +0100

    @Bean and @Component scan and @Primary

--- demo-spring-di/src/main/java/com/examples/spring/demo/config/DemoConfig.java
+++ demo-spring-di/src/main/java/com/examples/spring/demo/config/DemoConfig.java
@@ -1,15 +1,19 @@
 package com.examples.spring.demo.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import com.examples.spring.demo.todo.ITodo;
 import com.examples.spring.demo.todo.Todo;
 
 @Configuration
+@ComponentScan(basePackages = { "com.examples.spring.demo.todo" })
 public class DemoConfig {
 
 	@Bean
+	@Primary
 	public ITodo getTodo() {
 		return new Todo(1, "Default");
 	}
